a48624e337dbb30b64c12ef0a9dbb05a06d2d05c
AndrejWP <m2403371@edu.misis.ru>
2025-05-09 01:39:32 +0300

    Add files via upload

---
+++ White.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class White
+{
+    private string _input;
+    public string Input => _input;
+
+    protected White(string input)
+    {
+        _input = input;
+    }
+
+    public abstract void Review();
+}
